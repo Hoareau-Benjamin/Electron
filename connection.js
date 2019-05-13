b417cfc9d74d283b432e481bf3da9045068e67ea
Hoareau Benjamin <43531479+Hoareau-Benjamin@users.noreply.github.com>
2019-05-13 09:48:39 +0400

    Electron

---
+++ connection.js
@@ -0,0 +1,9 @@
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'ad_nazza',
+    password : 'pwnazza', 
+    database : 'nazza'
+});
